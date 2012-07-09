1b01ca2cc648e2cefd015a02992dd6788c405959
tlin-fei <tao.lin@feisystems.com>
2012-07-09 16:26:04 -0300

    added 2nd line

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,4 +1,3 @@
-
 public class Hello {
 
 	/**
@@ -6,6 +5,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+                   // The 2nd line
 		System.out.println("Hello World");
 	}
 
