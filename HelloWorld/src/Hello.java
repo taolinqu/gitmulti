8ee641ad76ded3835c05fde73e7cc2b297dab5d9
tlin-fei <tao.lin@feisystems.com>
2012-07-09 15:20:31 -0400

    Added 3rd line for comment

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// The 2nd line of comment
+		// This is the 3rd line of comment
 		System.out.println("Hello World");
 	}
 
