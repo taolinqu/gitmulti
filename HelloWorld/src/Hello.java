14c1a0556aeea502bdbc7194d2a6a817ca1ea8e8
tlin-fei <tao.lin@feisystems.com>
2012-07-09 15:22:56 -0400

    Removed some comments

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -6,8 +6,6 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// The 2nd line of comment
-		// This is the 3rd line of comment
 		System.out.println("Hello World");
 	}
 
