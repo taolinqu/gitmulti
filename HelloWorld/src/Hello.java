71afcd3e3da14314f791d61522880985d7f58817
Guy Number Two <two@two.com>
2012-12-27 10:25:15 -0500

    Reformat the Hello.java - Two

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -7,9 +7,9 @@ public class Hello {
 		// TODO Auto-generated method stub
 		// Added 2nd line comment
 		// The 2nd line
-                   // The 3rd line
-// The fourth line
-// 5th line is added
+		// The 3rd line
+		// The fourth line
+		// 5th line is added
 		// 6th line added in gitmulti-two
 		// 6th line added in gitmulti-one
 		System.out.println("Hello World");
