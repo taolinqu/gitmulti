efa01ce99e94790f2aa48fccaeedc035bade435c
tlin-fei <tao.lin@feisystems.com>
2012-12-31 15:23:26 -0500

    Get of ride 3 lines of comments

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -9,9 +9,6 @@ public class Hello {
 		// The 2nd line
 		// The 3rd line
 		// The fourth line
-		// 5th line is added
-		// 6th line added by One
-		// 7ht line for patch
 		System.out.println("Hello World");
 	}
 
