5e4ac39344607b017353ebabb097916cddf6771e
tlin-fei <tao.lin@feisystems.com>
2013-01-03 23:02:18 -0500

    Add lst line of comment by jessica

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// 1st line of comment by jessica
 		System.out.println("Hello World");
 	}
 
